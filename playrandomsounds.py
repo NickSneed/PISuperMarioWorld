6c02073cd309429e7130105c889bc9e7a8e5e992
NickSneed-Code <57041223+NickSneed-Code@users.noreply.github.com>
2023-10-08 12:58:31 -0500

    Update playrandomsounds.py

--- playrandomsounds.py
+++ playrandomsounds.py
@@ -1,14 +1,16 @@
 import random
 from aiy.voice.audio import play_wav
 from aiy.board import Board, Led
+from aiy.leds import Leds, Color
 
 TEST_SOUND_PATH = '/home/pi/GitHub/PIPlayRandomSounds/sounds/'
 
 def main():
 	with Board() as board:
-		board.led.state = Led.ON
-		play_wav(TEST_SOUND_PATH + 's1.wav')
-		board.led.state = Led.OFF
+		with Leds() as leds:
+			leds.update(Leds.rgb_pattern(Color.RED))
+			play_wav(TEST_SOUND_PATH + 's1.wav')
+			leds.update(Leds.rgb_off())
 
 		# While True will run forever
 		while True:
