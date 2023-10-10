95f7d41b921735342bbd1bf026fb3ceb0f043976
NickSneed-Code <57041223+NickSneed-Code@users.noreply.github.com>
2023-10-09 20:49:58 -0500

    added animations file

--- smw.py
+++ smw.py
@@ -11,6 +11,7 @@ from aiy.leds import Leds, Color, Pattern
 from aiy.voice.tts import say
 from smw.strings import strings
 from smw.actions import actions
+from smw.ledAnimations import *
 
 # Command args
 parser = argparse.ArgumentParser()
@@ -30,47 +31,6 @@ def printAndTalk(text):
 def talk(text):
 	if args.voice == 'True':
 		say(text)
-
-def flashAnimation(cycles):
-	
-	ledTime = 0.15
-
-	with Leds() as leds:
-
-		i = 0
-		while i < cycles:	
-			i += 1
-			leds.update(Leds.rgb_on(Color.GREEN))
-			time.sleep(ledTime)
-			leds.update(Leds.rgb_on(Color.BLUE))
-			time.sleep(ledTime)
-			leds.update(Leds.rgb_on(Color.YELLOW))
-			time.sleep(ledTime)
-			leds.update(Leds.rgb_on(Color.RED))
-			time.sleep(ledTime)
-			leds.update(Leds.rgb_on(Color.PURPLE))
-			time.sleep(ledTime)
-			leds.update(Leds.rgb_on(Color.WHITE))
-			time.sleep(ledTime)
-
-def fadeAnimation():
-	ledTime = 0.002
-
-	with Leds() as leds:
-
-		# Fade in
-		i = 0
-		while i < 256:
-			leds.update(Leds.rgb_on((i,0,0)))
-			i += 1
-			time.sleep(ledTime)
-
-		# Fade out
-		i = 255
-		while i > -1:
-			leds.update(Leds.rgb_on((i,0,0)))
-			i -= 1
-			time.sleep(ledTime)
 
 def main():
 	
---
+++ smw/ledAnimations.py
@@ -0,0 +1,44 @@
+import time
+from aiy.board import Board, Led
+from aiy.leds import Leds, Color, Pattern
+
+def flashAnimation(cycles):
+	
+	ledTime = 0.15
+
+	with Leds() as leds:
+
+		i = 0
+		while i < cycles:	
+			i += 1
+			leds.update(Leds.rgb_on(Color.GREEN))
+			time.sleep(ledTime)
+			leds.update(Leds.rgb_on(Color.BLUE))
+			time.sleep(ledTime)
+			leds.update(Leds.rgb_on(Color.YELLOW))
+			time.sleep(ledTime)
+			leds.update(Leds.rgb_on(Color.RED))
+			time.sleep(ledTime)
+			leds.update(Leds.rgb_on(Color.PURPLE))
+			time.sleep(ledTime)
+			leds.update(Leds.rgb_on(Color.WHITE))
+			time.sleep(ledTime)
+
+def fadeAnimation():
+	ledTime = 0.002
+
+	with Leds() as leds:
+
+		# Fade in
+		i = 0
+		while i < 256:
+			leds.update(Leds.rgb_on((i,0,0)))
+			i += 1
+			time.sleep(ledTime)
+
+		# Fade out
+		i = 255
+		while i > -1:
+			leds.update(Leds.rgb_on((i,0,0)))
+			i -= 1
+			time.sleep(ledTime)
